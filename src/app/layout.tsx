de9e2f113b17c875045397311a65816e50f734a8
fericopasaribu <fopsoft@gmail.com>
2025-07-18 02:12:16 +0700

    setup sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
+import { CustomSideBar } from "@/components/CustomSideBar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <SidebarProvider>
+          <CustomSideBar />
+          <main>
+            <div className="sm:hidden p-2">
+              <SidebarTrigger />
+            </div>
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
---
+++ src/components/CustomSideBar.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { Calendar, Home, Inbox, Search, Settings, X } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function CustomSideBar() {
+   const { open, toggleSidebar } = useSidebar();
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+         {open && (
+          <div className="flex justify-end p-2 w-full sm:hidden">
+            <button
+              onClick={toggleSidebar}
+              className="rounded-md p-2 hover:bg-muted w-full flex justify-end"
+              aria-label="Tutup Menu"
+            >
+              <X className="h-5 w-5" />
+            </button>
+          </div>
+        )}
+
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
