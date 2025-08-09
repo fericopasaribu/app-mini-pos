d68f0584a3a96ac53c418ea3fe22fda9e8c92b00
fericopasaribu <fopsoft@gmail.com>
2025-08-09 20:58:54 +0700

    remove migrations folder

--- prisma/migrations/20250723134808_03_tb_barang/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "tb_barang" DROP CONSTRAINT "tb_barang_id_satuan_fkey";
-
--- AddForeignKey
-ALTER TABLE "tb_barang" ADD CONSTRAINT "tb_barang_id_satuan_fkey" FOREIGN KEY ("id_satuan") REFERENCES "tb_satuan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20250802024623_07_tb_penjualan/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `kode` on the `tb_penjualan` table. The data in that column could be lost. The data in that column will be cast from `Char(20)` to `Char(15)`.
-
-*/
--- AlterTable
-ALTER TABLE "public"."tb_penjualan" ALTER COLUMN "kode" SET DATA TYPE CHAR(15);
